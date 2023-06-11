ee0bdcd319adbf9987cf08cec759c2c6ac7ed536
RomanBuzz <buninrom@gmail.com>
2023-06-11 22:23:02 +0300

    1

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = "6073254072:AAGPqiEsNJ54tCex91qWAWZvPVVVJHYXYgc"
+
+openexchanger_token = "53d5dcc6979a41b08b57d9a3ebd17651&"
+
+exchangerate_token = "9cdbbbb97950759f0581dd8189ae6c71&"
+
+keys = {
+    "рубль": "RUB",
+    "гривна": "UAH",
+    "доллар": "USD",
+    "евро": "EUR",
+    "юань": "CNY",
+    "йена": "JPY",
+    "биткоин": "BTC"
+}
